1f751f4ee3f88f6c2b68036301a11a018dd5e31a
Torsten Martinsen <torsten@bullestock.net>
2020-02-04 18:18:03 +0100

    Convert to module.

--- box/joystickcover.py
+++ box/joystickcover.py
@@ -4,27 +4,22 @@ from __future__ import division
 import os
 import sys
 import re
+from math import cos, radians, sin
 
 # Assumes SolidPython is in site-packages or elsewhwere in sys.path
 from solid import *
 from solid.utils import *
 
-SEGMENTS = 128
-
-def assembly():
+def joystick_cover(x, y, z):
     r1 = 16
     r2 = 15
     outer_s = sphere(r = r1)
     inner_s = sphere(r = r2)
+    # Spherical shell
     sph = outer_s - inner_s
-    tube_h = 3
-    hole1 = cylinder(r = 14, h = 25)
-    hole2 = cylinder(r = r2, h = 5)
-    tube = cylinder(r = 16, h = tube_h) - down(1)(cylinder(r = 14, h = tube_h+2))
+    # Top opening
+    hole1 = cylinder(r = 12, h = 20)
+    # Cube to cut off bottom
     block = translate([-20, -20, -20])(cube([40, 40, 20]))
-    flange = translate([0, 0, 0])(cylinder(r = 20, h = 2))
-    return flange + up(tube_h)(sph) + tube - block - down(1)(hole1) - down(1)(hole2)
-
-if __name__ == '__main__':
-    a = assembly()
-    scad_render_to_file(a, file_header='$fn = %s;' % SEGMENTS, include_orig_code=False)
+    a = sph - block - down(1)(hole1)
+    return translate([x, y, z])(a)
