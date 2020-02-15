85fdc8709d86046760fa6e1802ae483342c2ff58
Torsten Martinsen <torsten@bullestock.net>
2020-02-15 01:22:48 +0100

    Add PCB supports and studs.

--- box/bottom.py
+++ box/bottom.py
@@ -16,6 +16,9 @@ SEGMENTS = 32
 
 e = 0.001
 
+# TODO: determine proper height
+pcb_h = 5
+
 center_x = 107.500 + 2.5
 center_x_pcb = (159.766 + 90.424)/2
 
@@ -40,7 +43,7 @@ hole_h = 50
 pcb_z = 10
 
 def cylinder_at(x, y, d, h):
-    return translate([x, y, oah - th - 17])(cylinder(d = d, h = h))
+    return translate([x, y, oah-h-th])(cylinder(d = d, h = h))
 
 # Cube centered in x/y
 def c2cube(w, h, d):
@@ -49,18 +52,23 @@ def c2cube(w, h, d):
 def holes():
     return cylinder_at(0, 0, 0, 0)
 
-def map(y):
+def map_y(y):
     return 195.5 - y 
 
-def pcbsupport(x, y):
-    return cylinder_at(x - pcb_x_offset, map(y), 4, 18)
+def map_x(x):
+    return -(x - pcb_x_offset)
 
+def pcbsupport(x, y):
+    return cylinder_at(map_x(x), map_y(y), th, pcb_h)
+
+# Recessed
 def screwstud(x, y):
     o = translate([x, y, oah - th - 7])(cylinder(d = 8, h = 8))
     i = translate([x, y, oah - th - 8])(cylinder(d = 3, h = 12))
     recess = translate([x, y, oah - 3.5])(cylinder(d = 6, h = 10))
     return o - hole()(i + recess)
 
+# Flush
 def screwstud2(x, y):
     d = 2
     o = translate([x, y, 0])(cylinder(d = 8, h = oah-d))
@@ -68,10 +76,33 @@ def screwstud2(x, y):
     recess = translate([x, y, oah - 3.5])(cylinder(d = 6, h = 10))
     return o + hole()(i + recess)
 
+# for PCB
+def screwstud3(x, y):
+    o = translate([map_x(x), map_y(y), oah - th - pcb_h])(cylinder(d = 8, h = pcb_h + 1))
+    i = translate([map_x(x), map_y(y), -th])(cylinder(d = 2, h = oah))
+    return o + hole()(i)
+
 def pcbmounts():
-    #!!
-    p1 = pcbsupport(52, 217)
-    return p1
+    p1 = pcbsupport(50, 176)
+    p2 = pcbsupport(44, 198)
+    p3 = pcbsupport(202, 176)
+    p4 = pcbsupport(203, 198)
+    p5 = pcbsupport(54, 140)
+    p6 = pcbsupport(69, 130)
+    p7 = pcbsupport(87, 140)
+    p8 = pcbsupport(80, 192)
+    p9 = pcbsupport(86, 217)
+    p10 = pcbsupport(126, 204)
+    p11 = pcbsupport(126, 228)
+    p12 = pcbsupport(166, 217)
+    p13 = pcbsupport(166, 135)
+    p14 = pcbsupport(199, 140)
+    p15 = pcbsupport(182, 132)
+    s1 = screwstud3(60.452, 156.972)
+    s2 = screwstud3(52.578, 225.552)
+    s3 = screwstud3(176.53, 166.878)
+    s4 = screwstud3(191.008, 225.552)
+    return s1 + s2 + s3 + s4 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 + p11 + p12 + p13 + p14 + p15
 
 def screwstuds():
     s2 = screwstud(-92, -5) + screwstud(89, 10)
