e71b0d9272e1b2b61498f39dfe380165326fb4ca
Torsten Martinsen <torsten@bullestock.net>
2024-05-06 21:48:03 +0200

    Possibly fix ADC read.

--- esp32/main/hw.cpp
+++ esp32/main/hw.cpp
@@ -8,11 +8,11 @@
 #include <driver/i2c_master.h>
 
 // 1001000
-const int i2c_address = 0x48;
+const int ADC_ADDRESS = 0x48;
 const int SINGLE_ENDED = 0x80;
-const int IREF_OFF_AD_ON = 0x04; // Internal reference OFF, A/D converter ON
+const int IREF_ON_AD_ON = 0x0C; // Internal reference ON, A/D converter ON
 
-i2c_master_bus_handle_t i2c_bus_handle;
+i2c_master_dev_handle_t adc_handle;
 
 void init_hardware()
 {
@@ -43,7 +43,7 @@ void init_hardware()
     ESP_ERROR_CHECK(gpio_config(&io_conf));
 
     i2c_master_bus_config_t i2c_mst_config = {
-        .i2c_port = -1,
+        .i2c_port = I2C_NUM_0,
         .sda_io_num = PIN_SDA,
         .scl_io_num = PIN_SCL,
         .clk_source = I2C_CLK_SRC_DEFAULT,
@@ -53,30 +53,31 @@ void init_hardware()
     };
     i2c_mst_config.flags.enable_internal_pullup = true;
 
+    i2c_master_bus_handle_t i2c_bus_handle;
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &i2c_bus_handle));
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = ADC_ADDRESS,
+        .scl_speed_hz = 100000,
+    };
+
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &dev_cfg, &adc_handle));
 }
 
 int read_adc(int channel)
 {
-    i2c_device_config_t dev_cfg = {
-        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = i2c_address,
-        .scl_speed_hz = 100000,
-    };
-
-    i2c_master_dev_handle_t dev_handle;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &dev_cfg, &dev_handle));
     channel = channel & 0x7;
-    const uint8_t csel = (channel >> 1) | ((channel & 0x1) << 2);
-    const uint8_t data = SINGLE_ENDED | csel << 4 | IREF_OFF_AD_ON;
+    //const uint8_t csel = (channel >> 1) | ((channel & 0x1) << 2);
+    const uint8_t data = SINGLE_ENDED | channel << 4 | IREF_ON_AD_ON;
     uint8_t value[2];
-    auto res = i2c_master_transmit_receive(dev_handle, &data, 1, value, 2, 100);
+    auto res = i2c_master_transmit_receive(adc_handle, &data, 1, value, 2, 100);
     ESP_ERROR_CHECK(res);
     //printf("i2c: %d\n", res);
     
     return value[0] + value[1] * 256;
 }
 
-// Variables:
+// Local Variables:
 // compile-command: "cd .. && idf.py build"
 // End:
