299ddfcd5603bc6db928ad05c8f765079053ba42
Torsten Martinsen <torsten@bullestock.net>
2024-05-10 12:17:27 +0200

    Spacer for display header.

---
+++ box/displayspacer.py
@@ -0,0 +1,20 @@
+import cadquery as cq
+
+res = (cq.Workplane("XY")
+       .rarray(1, 5*2.54, 1, 2)
+       .rect(2.54*3, 3)
+       .extrude(5)
+       .edges(">Z")
+       .fillet(0.23)
+       .faces(">Z")
+       .workplane()
+       .rect(2.54, 2.54*6)
+       .extrude(7)
+       .edges("|Z")
+       .fillet(0.1)
+       .rarray(1, 2.54, 1, 4)
+       .circle(0.7)
+       .cutThruAll()
+)
+
+show_object(res)
