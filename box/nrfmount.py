c0752df37d80b9a11bad94bfacd7aa53f94fece8
Torsten Martinsen <torsten@bullestock.net>
2024-05-18 09:44:47 +0200

    Fix dimensions.

--- box/nrfmount.py
+++ box/nrfmount.py
@@ -12,24 +12,23 @@ from solid.utils import *
 SEGMENTS = 32
 
 w1 = 15.5
-h1 = 8.5 + 2.5
-l1 = 10
+h1 = 8.5 + 2
+l1 = 15
 h2 = 3
-l2 = 15.5+1
+l2 = 14
 
 def block():
     return translate([-w1/2, 0, 0])(cube([w1, l1, h1]) -
-                                    translate([w1/2, l1/2, -1])(cylinder(h = 20, d = 3)))
+                                    translate([w1/2 - 0, l1/2 - 0, -1])(cylinder(h = 20, d = 2.9)))
 
 def tab():
-    s = hole()(cylinder(h = 5, d = 1.5))
-    d = 11.4
+    s = hole()(cylinder(h = 5, d = 1))
     d2 = 12
     sw = 8
     return translate([-w1/2, 0, 0])(cube([w1, l2, h2]) -
-                                    translate([sw/2, 4, -1])(cube([sw, 15, h2+2])) +
+                                    translate([(sw-1)/2, 4, -1])(cube([sw+1, 15, h2+2])) +
                                     translate([w1/2 - d2/2, l2 - 1.5, 0])(s) +
-                                    translate([w1/2 + d/2, l2 - 1.5, 0])(s))
+                                    translate([w1/2 + d2/2, l2 - 1.5, 0])(s))
 
 def assembly():
     b = block()
