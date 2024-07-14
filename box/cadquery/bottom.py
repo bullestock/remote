d3dc55b077954f8c8c91cd8a41d93cf6f8bb0105
Torsten Martinsen <torsten@bullestock.net>
2024-07-14 17:24:41 +0200

    Carve out interior.

--- box/cadquery/bottom.py
+++ box/cadquery/bottom.py
@@ -1,4 +1,5 @@
 from cadquery import exporters
+import copy, math
 
 # Overall height
 oah = 27
@@ -6,6 +7,21 @@ hole_h = 50
 pcb_z = 10
 th = 3
 
+def sign(x):
+    return -1 if x < 0 else 1
+
+def make_inset(p):
+    x, y = p[0], p[1]
+    length = math.sqrt(x*x + y*y)
+    if x == 0:
+        angle = math.pi/2 # if y > 0 else -math.pi/2
+    else:
+        angle = math.atan(abs(y/x))
+    length -= th
+    x = math.cos(angle) * sign(x) * length
+    y = math.sin(angle) * sign(y) * length
+    return (x, y)
+    
 # Half of the shape
 points = [ (0, -35),
            (20, -35), 
@@ -21,25 +37,25 @@ points = [ (0, -35),
            (0, 108) ]
 
 # Add mirrored half
-outer_points = points
+outer_points = copy.copy(points) # break reference
 for i in range(len(points) - 2, 0, -1):
     outer_points.append((-points[i][0], points[i][1]))
 
 # Create inset points
 inner_points = []
 for p in points:
-    inner_points.append((p[0], p[1]))
+    inner_points.append(make_inset(p))
 for i in range(len(points) - 2, 0, -1):
-    inner_points.append((-points[i][0], points[i][1]))
+    inner_points.append(make_inset((-points[i][0], points[i][1])))
 
 outer_spline = cq.Workplane("XY").spline(outer_points).close()
 
 outer = outer_spline.extrude(oah).faces("<Z").fillet(1.35)
 
-#inner_spline = cq.Workplane("XY").workplane(th).spline(inner_points).close()
+inner_spline = cq.Workplane("XY").workplane(th).spline(inner_points).close()
 
-#inner = inner_spline.extrude(oah).faces("<Z").fillet(1)
+inner = inner_spline.extrude(oah)
 
-result = outer#.cut(inner)
+result = outer.cut(inner)
 
 show_object(result)
