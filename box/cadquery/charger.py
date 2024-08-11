ef8f535ab21aa4bed094dab51693817d06dbcb5e
Torsten Martinsen <torsten@bullestock.net>
2024-08-11 11:42:38 +0200

    Mount for charger module.

---
+++ box/cadquery/charger.py
@@ -0,0 +1,28 @@
+charger_d1 = 17.5
+charger_d2 = 27.8
+charger_d3 = 3
+charger_d4 = 1.5
+charger_x = 0
+charger_y = 55
+charger_z = 4
+th = 3
+
+result = (cq.Workplane("XY")
+          .box(charger_d1 + 2*charger_d4,
+               charger_d2 + 2*charger_d4,
+               charger_z + charger_d3,
+               centered=(True, True, False))
+          )
+
+charger_inner = (cq.Workplane().
+          box(charger_d1, charger_d2, charger_d3, centered=(True, True, False)))
+
+plug_cutout = (cq.Workplane().
+          box(15, charger_d2 + 5, charger_d3, centered=(True, True, False)))
+
+result = result - charger_inner.translate((0, 0, charger_z))
+
+result = result - plug_cutout.translate((0, -5, charger_z))
+
+show_object(result)
+
