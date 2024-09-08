f1c9e2bcc9b30f372015af0a8486a29e7a66f927
Torsten Martinsen <torsten@bullestock.net>
2024-09-08 14:41:43 +0200

    Fix memcpy.

--- esp32/main/main.cpp
+++ esp32/main/main.cpp
@@ -71,13 +71,15 @@ void app_main(void)
     unsigned long failures = 0;
     unsigned long crc_errors = 0;
     unsigned long successes = 0;
-    bool show_error = false;
-    uint16_t their_battery = 0;
+    float their_battery = 0.0;
 
     const int NOF_DELAY_SAMPLES = 10;
     int64_t delay_samples[NOF_DELAY_SAMPLES];
     int actual_delay_samples = 0;
 
+    display.clear();
+    display.add_progress("Ready");
+    int count = 0;
     while (1)
     {
         const auto send_time = esp_timer_get_time();
@@ -86,23 +88,39 @@ void app_main(void)
 
         const auto my_battery = get_my_battery();
 
-#if 1
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        display.clear();
-        display.add_progress(format("L %3d %3d",
-                                    frame.left_x, frame.left_y));
-        display.add_progress(format("R %3d %3d",
-                                    frame.right_x, frame.right_y));
-        display.add_progress(format("B %02X T %02X S %d",
-                                    frame.pushbuttons,
-                                    frame.toggles,
-                                    frame.slide));
-        display.add_progress(format("P %02X %02X",
-                                    frame.left_pot, frame.right_pot));
-        display.add_progress(format("B %.3f", my_battery));
+        if (++count > 100)
+        {
+            count = 0;
+            printf("Update display\n");
+#if 0
+            display.clear();
+            display.add_progress(format("L %3d %3d",
+                                        frame.left_x, frame.left_y));
+            display.add_progress(format("R %3d %3d",
+                                        frame.right_x, frame.right_y));
+            display.add_progress(format("B %02X T %02X S %d",
+                                        frame.pushbuttons,
+                                        frame.toggles,
+                                        frame.slide));
+            display.add_progress(format("P %02X %02X",
+                                        frame.left_pot, frame.right_pot));
+            display.add_progress(format("B %.3f", my_battery));
+#else
+            display.clear_status();
 #endif
-        // Normal operation
-        //display.add_progress(format("%d %d", successes, failures));
+            int delay = 0;
+            if (actual_delay_samples > 0)
+            {
+                int64_t sum = 0;
+                for (int i = 0; i < actual_delay_samples; ++i)
+                    sum += delay_samples[i];
+                delay = sum/actual_delay_samples;
+            }
+            display.add_progress(format("OK %d F %d", successes, failures));
+            display.add_progress(format("Bad CRC %d", crc_errors));
+            display.add_progress(format("Delay %d", delay));
+            display.add_progress(format("Bat %.3f/%.2f", my_battery, their_battery));
+        }
         
         bool ok = send_frame(nrf24, frame);
         if (ok)
@@ -110,16 +128,14 @@ void app_main(void)
         else
             ++consecutive_errors;
 
-        display.add_progress(format("Send %d", ok));
-
-        bool show_error = (consecutive_errors > 10);
+        //bool show_error = (consecutive_errors > 10);
 
         if (ok)
         {
             ++successes;
 
             bool ready = false;
-            for (int i = 0; i < 100; ++i)
+            for (int i = 0; i < 1000; ++i)
             {
                 if (data_ready(nrf24))
                 {
@@ -135,22 +151,29 @@ void app_main(void)
                 uint8_t data[sizeof(ForwardAirFrame)];
                 Nrf24_getData(&nrf24, data);
                 ReturnAirFrame ret_frame;
-                memcpy(data, &ret_frame, sizeof(ret_frame));
-                if ((ret_frame.magic == ReturnAirFrame::MAGIC_VALUE) &&
-                    check_crc(ret_frame))
+                memcpy(&ret_frame, data, sizeof(ret_frame));
+                if (ret_frame.magic != ReturnAirFrame::MAGIC_VALUE)
+                {
+                    printf("Bad magic: %04X\n", ret_frame.magic);
+                    ++failures;
+                }
+                else if (!check_crc(ret_frame))
+                {
+                    printf("Bad CRC\n");
+                    ++crc_errors;
+                }
+                else
                 {
                     const auto end_time = esp_timer_get_time();
 
                     for (int i = actual_delay_samples-1; i > 0; --i)
                         delay_samples[i] = delay_samples[i-1];
                     delay_samples[0] = end_time - send_time;
+                    printf("DELAY %lld\n", delay_samples[0]);
+                    if (actual_delay_samples < NOF_DELAY_SAMPLES)
+                        ++actual_delay_samples;
 
-                    their_battery = ret_frame.battery;
-                }
-                else
-                {
-                    ++crc_errors;
-                    show_error = true;
+                    their_battery = ret_frame.battery/1000.0;
                 }
             }
         }
